59cb6a3f1c95bc37ef095597c438bc7098723a88
Luiz Eduardo Angelo Marinho <89088292+EduardoAngelo2803@users.noreply.github.com>
2022-10-25 14:04:35 -0300

    First

---
+++ rate_leam.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+
+    int id;
+    int timeDur;
+    int timeExec;
+    int timeWait;
+
+}Process;
+
+int main (int argc, char *argv[]) {
+
+    Process *list;
+
+
+    return 0;
+}
