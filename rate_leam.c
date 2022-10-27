cfe09fa7e918df1e872a5bdc50cc913a644e16d2
Luiz Eduardo Angelo Marinho <89088292+EduardoAngelo2803@users.noreply.github.com>
2022-10-27 08:33:17 -0300

    Tests

--- rate_leam.c
+++ rate_leam.c
@@ -1,20 +1,42 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 3
+
+
 
 typedef struct {
 
-    int id;
+    char id[SIZE];
     int timeDur;
     int timeExec;
     int timeWait;
 
-}Process;
+} Process;
 
-int main (int argc, char *argv[]) {
+Process list[SIZE];
 
-    Process *list;
+void calcular () {
 
+    
+
+    printf("Processo 1: \n");
+
+    scanf("%19[0-9a-zA-Z]", list[0].id);
+    scanf("%d", &list[0].timeExec);
+    scanf("%d", &list[0].timeWait);
+
+    printf("Processo 2: \n");
+   // fgets(list[1].id, 2, stdin);
+    scanf("%d", &list[1].timeExec);
+    scanf("%d", &list[1].timeWait);
+}
+
+int main () {
+
+    calcular();
+
+    printf("%s\n", list[0].id);
 
     return 0;
 }
